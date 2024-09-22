dec26371043ab827477721a789be875edd16fb1e
Aleyna ÇELİK <77541289+AleynaaCelik@users.noreply.github.com>
2024-09-22 23:48:10 +0300

    Implement Database class for MongoDB connection

--- api/db/Database.js
+++ api/db/Database.js
@@ -1,20 +1,28 @@
-const mongoose=require("mongoose");
+const mongoose = require("mongoose");
 
-let instance=null;
-class Database{
+let instance = null;
 
-    constructor(){
-        if(!instance){
-            this.mongoConnection=null;
-            instance=this;
+class Database {
+    constructor() {
+        if (!instance) {
+            this.mongoConnection = null;
+            instance = this;
         }
         return instance;
     }
-    async conect(options){
-        let db=await mongoose.connect(options.CONNECTION_STRING);
 
-        this.mongoConnection=db;
+    async connect(options) {  // Metod ismi düzeltildi
+        try {
+            console.log("Db CONNECTING...");
+            let db = await mongoose.connect(options.CONNECTION_STRING);
+
+            this.mongoConnection = db;
+            console.log("Db CONNECTED!");
+        } catch (err) {
+            console.error(err);
+            process.exit(1);
+        }
     }
 }
 
-module.exports=Database;
+module.exports = Database;
